8d5bf64617eeabbc00a257870a687bb4abc10784
pPlanello <planello96@gmail.com>
2022-01-22 19:53:01 +0100

    Reading environment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 // Create server with express
 const app = express();
@@ -13,6 +14,6 @@ app.use( express.json() );
 // Routes
 app.use( '/api/auth', require('./routes/auth') );
 
-app.listen(4000, () => {
-    console.log(`Servidor corriendo en puerto ${ 4000 }`)
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
 });
